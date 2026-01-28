6215b77e296541b626bd873c1144faf75a92bfa2
Ahmed-Satoru <ahmedweb2007@gmail.com>
2026-01-28 15:20:36 +0000

    added some styles

--- learning-lab/src/projects/BusinessVocabulary/VocabTable.tsx
+++ learning-lab/src/projects/BusinessVocabulary/VocabTable.tsx
@@ -1,6 +1,7 @@
 import './vocabTable.css';
 import { businessWords, categories } from "../../data/BusinessVocabulary/words.ts";
 import { tableTitles } from '../../data/BusinessVocabulary/tables.ts';
+import { Fragment } from 'react';
 
 
 
@@ -12,49 +13,44 @@ export default function VocabTable() {
         <>
             <div className='title'>Vocabulary Table Component</div>
             
-                {categories.map( (cat, index) => (
-                    <>
-                        <div className='category-container' key={index}>
+            {categories.map( (cat) => (
+                <Fragment key={cat}>
+                    <section key={cat} className={`${cat}-section`}>
+                        {/* CATEGORY LINE */}
+                        <div className='category-container'>
                             <div className='category-name'>{cat}</div>
                         </div>
-                        
-                        
-            <table>  
-                <thead>
-                    <tr>
-                        { tableTitles.map((title, i) => <th key={i}>{title}</th>) }
-                    </tr>
-                </thead>
-                <tbody>
-
-                    { businessWords.filter(word=> word.category === cat)
-                    .map((word) => (
-                        <>
-                           
-                            <tr className='word-row' key={word.id}>
-                                <td className={`en-word ${
-                                    word.frequency.type === 'high' ? 'high-freq' :
-                                    word.frequency.type === 'medium' ? 'med-freq' :
-                                    word.frequency.type === 'low' ? 'low-freq' :
-                                    word.frequency.type === 'rare' ? 'rare-freq' : ''
-                                    }`}>
-                                        {word.word}
-                                </td>
-                                <td className='meaning'>{word.meaning}</td>
-                                <td className='defAr'>{word.defAr}</td>
-                                <td className='defEn'>{word.defEn}</td>
-                            </tr> 
-                        </>
-                        ))
-                    }
-                </tbody>
+                        {/* WORDS TABLE */}
+                        <div className=''>
+                            <table className='fixed-table'>  
+                            {/* TABLE HEAD */}
+                            <thead>
+                                <tr>
+                                    { tableTitles.map((title, i) => <th key={i}>{title}</th>) }
+                                </tr>
+                            </thead>
+                            {/* TABLE BODY */}
+                            <tbody>
+                                { businessWords
+                                .filter(word=> word.category === cat)
+                                .map((word) => (    
+                                        <tr className='word-row' key={word.id}>
+                                            <td className={`en-word ${word.frequency.type}-freq`}>
+                                                    {word.word}
+                                            </td>
+                                            <td className='meaning'>{word.meaning}</td>
+                                            <td className='defAr'>{word.defAr}</td>
+                                            <td className='defEn'>{word.defEn}</td>
+                                        </tr>  
+                                    ))
+                                }
+                            </tbody>
+                            </table>
+                        </div>
+                    </section>
+                </Fragment>
                 
-            </table>
-                    </>
-                    
-                ))}
-            
-
+            ))}
         </>
     )
 }
